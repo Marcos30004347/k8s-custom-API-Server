53200522040c505ef7cfb3fac19c918c742486c4
Marcos Vinicius <marcosvinicius@MacBook-Pro-de-Marcos.local>
2021-03-06 14:53:57 -0300

    [feat, not tested]: add custom controller for the foo resource

--- main.go
+++ main.go
@@ -3,23 +3,90 @@ package main
 import (
 	"flag"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/spf13/cobra"
 
 	genericapiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/Marcos30004347/k8s-custom-API-Server/pkg/cmd/server"
+	"github.com/Marcos30004347/k8s-custom-API-Server/pkg/controller"
+
 	"k8s.io/component-base/logs"
 	"k8s.io/klog"
+
+	clientset "github.com/Marcos30004347/k8s-custom-API-Server/pkg/generated/clientset/versioned"
+	informers "github.com/Marcos30004347/k8s-custom-API-Server/pkg/generated/informers/externalversions"
+	kubeinformers "k8s.io/client-go/informers"
 )
+
+var (
+	stopCh        <-chan struct{}
+	apiController *controller.Controller
+	command       *cobra.Command
+)
+
+func runController(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	if err := apiController.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
+
+func runServerAPI(wg *sync.WaitGroup) {
+	if err := command.Execute(); err != nil {
+		klog.Fatal(err)
+	}
+}
 
 func main() {
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
-	stopCh := genericapiserver.SetupSignalHandler()
+	stopCh = genericapiserver.SetupSignalHandler()
 	options := server.NewCustomServerOptions(os.Stdout, os.Stderr)
-	cmd := server.NewCommandStartCustomServer(options, stopCh)
-	cmd.Flags().AddGoFlagSet(flag.CommandLine)
-	if err := cmd.Execute(); err != nil {
-		klog.Fatal(err)
+
+	command = server.NewCommandStartCustomServer(options, stopCh)
+	command.Flags().AddGoFlagSet(flag.CommandLine)
+
+	cfg, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
 	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	exampleClient, err := clientset.NewForConfig(cfg)
+
+	if err != nil {
+		klog.Fatalf("Error building example clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	sharedInformer := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+	apiController = controller.NewController(kubeClient, exampleClient,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		sharedInformer.Baz().V1alpha1().Foos())
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(stopCh)
+	sharedInformer.Start(stopCh)
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go runServerAPI(&wg)
+	go runController(&wg)
+
+	wg.Wait()
 }
